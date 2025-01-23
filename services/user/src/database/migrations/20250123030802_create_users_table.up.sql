524ba3e752494c855ec36bf41c64f291af366678
levensspel <levensspiel@gmail.com>
2025-01-23 18:54:31 +0800

    fix: column type changes

--- services/user/src/database/migrations/20250123030802_create_users_table.up.sql
+++ services/user/src/database/migrations/20250123030802_create_users_table.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     password_hash TEXT NOT NULL,
     fileId TEXT,
     fileUri TEXT,
-    fileThumbnailUri VARCHAR(255),
+    fileThumbnailUri TEXT,
     bankAccountName VARCHAR(255) CHECK (length(bankAccountName) >= 4 AND length(bankAccountName) <= 32),
     bankAccountHolder VARCHAR(255) CHECK (length(bankAccountHolder) >= 4 AND length(bankAccountHolder) <= 32),
     bankAccountNumber VARCHAR(255) CHECK (length(bankAccountNumber) >= 4 AND length(bankAccountNumber) <= 32),
