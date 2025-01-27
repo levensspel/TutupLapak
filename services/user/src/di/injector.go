26260715866ac6682e7bf697444369978f5d2fba
prasasdi <rizky.adie7@gmail.com>
2025-01-27 10:57:11 +0700

    chore tidy

--- services/user/src/di/injector.go
+++ services/user/src/di/injector.go
@@ -42,8 +42,6 @@ func init() {
 	do.Provide[userController.UserControllerInterface](Injector, userController.NewUserControllerInject)
 
 	//? Proto User Controller
-	// do.Provide[protoUserController.ProtoUserController](Injector, protoUserController.NewUserControllerInject)
-	// do.Provide[protoUserController.ProtoUserController](Injector, protoUserController.NewUserControllerInject)
 	do.Provide[*protoUserController.ProtoUserController](Injector, protoUserController.NewUserControllerInject)
 
 	//? Setup Services
