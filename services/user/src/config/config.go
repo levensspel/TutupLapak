a8d3b26575bb67d3547475e6d543426749345bbe
levensspel <levensspiel@gmail.com>
2025-02-01 10:00:19 +0800

    chore: add db max conn usage to env var

--- services/user/src/config/config.go
+++ services/user/src/config/config.go
@@ -66,6 +66,18 @@ func GetRedisDbCount() int {
 	return count
 }
 
+// Percentage use of max database connections.
+// Default to 0.8 (80%).
+// Range: 0.0 < x <= 10.
+func GetDbMaxConnPercentage() float64 {
+	percentage, err := strconv.ParseFloat(getEnv("DB_MAX_CONN_PERCENTAGE", "0.8"), 64)
+	if err != nil || percentage <= 0.0 || percentage > 1.0 {
+		panic("Invalid DB_MAX_CONN_PERCENTAGE: 0.0 < value <= 1.0")
+	}
+
+	return percentage
+}
+
 func getFileServiceBaseURL() string {
 	return getEnv("FILE_SERVICE_BASE_URL", "")
 }
