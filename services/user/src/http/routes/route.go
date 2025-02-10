ed46272a4767a7b10fa63e6846a9fc7605028f51
levensspel <levensspiel@gmail.com>
2025-02-10 18:03:18 +0800

    feat: add health check API & move monitor API to route file

--- services/user/src/http/routes/route.go
+++ services/user/src/http/routes/route.go
@@ -1,8 +1,27 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"strings"
+	"time"
+
+	"github.com/TIM-DEBUG-ProjectSprintBatch3/TutupLapak/user/src/config"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+)
 
 func SetRoutes(app *fiber.App) fiber.Router {
+	if strings.ToUpper(config.MODE) == config.MODE_DEBUG {
+		app.Get("/monitor", monitor.New(monitor.Config{
+			Title:   "User Service Metrics",
+			Refresh: 5 * time.Second,
+			APIOnly: false,
+		}))
+	}
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK)
+	})
+
 	route := app.Group("/v1")
 
 	return route
